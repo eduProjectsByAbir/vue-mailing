cf9df4a31d27444252ed341561db60b47a5f3403
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-01-25 19:53:39 +0600

    changeds

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-
+<div class="container">
+    <div class="mail-box">
+        <app-sidebar></app-sidebar>
+    </div>
+</div>
 </template>
 
 <script>
+import Sidebar from './Sidebar.vue';
 export default {
+    components: {
+        appSidebar: Sidebar
+    }
 
 }
 </script>
