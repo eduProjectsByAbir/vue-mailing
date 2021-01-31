e4f71c7e46e53e11b92595b035f681dd0a370167
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-01-31 16:08:52 +0600

    fixed and shown inbox

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 <div class="container">
     <div class="mail-box">
         <app-sidebar :messages="messages"></app-sidebar>
-        <app-content></app-content>
+        <app-content :messages="messages"></app-content>
     </div>
 </div>
 </template>
 
 <script>
-import Sidebar from './Sidebar .vue';
+import Sidebar from './Sidebar.vue';
 import Content from './Content.vue';
 import messages from './data/messages';
 
