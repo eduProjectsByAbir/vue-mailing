341271c33ac186aa3f1454995e5689abed4cb942
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-02-03 15:17:29 +0600

    adding last files

--- src/Content.vue
+++ src/Content.vue
@@ -1,68 +1,73 @@
 <template>
-    <aside class="lg-side">
-        <div class="inbox-head">
-            <h3>{{ currentView.title }}</h3>
-        </div>
-        <keep-alive>
-            <component :is="currentView.tag" :data="currentView.data"></component>
-        </keep-alive>
-    </aside>
+  <aside class="lg-side">
+    <div class="inbox-head">
+      <h3>{{ currentView.title }}</h3>
+    </div>
+    <keep-alive>
+      <component :is="currentView.tag" :data="currentView.data"></component>
+    </keep-alive>
+  </aside>
 </template>
 
 <script>
-import Inbox from './Inbox.vue';
-import Sent from './Sent.vue';
-import Important from './Important.vue';
-import Trash from './Trash.vue';
-import ViewMessage from './ViewMessage.vue';
-import { eventBus } from './main';
+import Inbox from "./Inbox.vue";
+import Sent from "./Sent.vue";
+import Important from "./Important.vue";
+import Trash from "./Trash.vue";
+import ViewMessage from "./ViewMessage.vue";
+import { eventBus } from "./main";
 
 export default {
-    props: {
-        messages: {
-            type: Array,
-            required: true
-        }
+  props: {
+    messages: {
+      type: Array,
+      required: true,
     },
+  },
 
-    created(){
-        eventBus.$on('changeView', (data) => {
-                let temp = [{
-                    tag: data.tag,
-                    title: data.title,
-                    data: data.data || {}
-            }];
+  created() {
+    eventBus.$on("changeView", (data) => {
+      let temp = [
+        {
+          tag: data.tag,
+          title: data.title,
+          data: data.data || {},
+        },
+      ];
 
-            this.history = temp.concat(this.history.splice(0));
-        });
-    },
+      this.history = temp.concat(this.history.splice(0));
+    });
+  },
 
-    data(){
-        return {
-            history: [
-                {
-                tag: 'app-inbox',
-                title: 'Inbox',
-                data: {
-                    messages: null
-                }
-            }
-            ]
-        };
+  data() {
+    return {
+      history: [
+        {
+          tag: "app-inbox",
+          title: "Inbox",
+          data: {
+            messages: null,
+          },
+        },
+      ],
+    };
+  },
+  computed: {
+    currentView() {
+      let current = this.history[0];
+      current.data.messages = this.messages;
+      return current;
     },
-    computed: {
-        currentView () {
-            let current = this.history[0];
-            current.data.messages = this.messages;
-            return current;
-        }
+    previousView() {
+      return typeof this.history[1] !== "undefined" ? this.history[1] : null;
     },
-    components: {
-        appInbox: Inbox,
-        appSent: Sent,
-        appImportant: Important,
-        appTrash: Trash,
-        appViewMessage: ViewMessage
-    }
-}
+  },
+  components: {
+    appInbox: Inbox,
+    appSent: Sent,
+    appImportant: Important,
+    appTrash: Trash,
+    appViewMessage: ViewMessage,
+  },
+};
 </script>
--- src/ViewMessage.vue
+++ src/ViewMessage.vue
@@ -1,49 +1,94 @@
 <template>
-    <div class="inbox-body">
+  <div class="inbox-body">
+    <button
+      class="btn btn-danger pull-right"
+      @click="data.message.isDeleted = true"
+      :disabled="data.message.isDeleted"
+    >
+      <i class="fa fa-trash-o"></i> &nbsp;
+      {{ data.message.isDeleted ? "Deleted" : "Delete" }}
+    </button>
+    <template v-if="typeof data.message.isRead !== 'undefined'">
+      <button
+        class="btn btn-warning pull-right"
+        @click="data.message.isRead = false"
+        :disabled="!data.message.isRead"
+      >
+        <i class="fa fa-envelope-open"></i> &nbsp;
+      </button>
+      <button
+        class="btn btn-success pull-right"
+        @click="data.message.isRead = true"
+        :disabled="data.message.isRead"
+      >
+        <i class="fa fa-envelope"></i> &nbsp;
+      </button>
+    </template>
+    <button class="btn btn-primary pull-right" @click="navigateBack">
+      <i class="fa fa-arrow-left" aria-hidden="true"></i> &nbsp; Back
+    </button>
     <p><strong>Date:</strong> {{ data.message.date.fromNow() }}</p>
     <p><strong>Subject:</strong> {{ data.message.from.name }}</p>
-    <p><strong>Subject:</strong> {{ data.message.from.name }} <{{ data.message.from.email }}></p>
-    <hr>
+    <p>
+      <strong>Subject:</strong> {{ data.message.from.name }} <{{
+        data.message.from.email
+      }}>
+    </p>
+    <hr />
     <div v-html="data.message.content" class="message"></div>
-        <div v-if="data.message.attachments.length > 0" class="attachments">
-            <h4>Attachments</h4>
-            <ul>
-                <li v-for="attachment in data.message.attachments">
-                    <i class="fa fa-paperclip"></i> {{ attachment.fileName }} ({{ attachment.size | formatBytes }})
-                </li>
-            </ul>
-        </div>
-
+    <div v-if="data.message.attachments.length > 0" class="attachments">
+      <h4>Attachments</h4>
+      <ul>
+        <li v-for="attachment in data.message.attachments">
+          <i class="fa fa-paperclip"></i> {{ attachment.fileName }} ({{
+            attachment.size | formatBytes
+          }})
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
+import { eventBus } from "./main";
+
 export default {
-    props: {
-        data: {
-            type: Object,
-            required: true
-        }
+  props: {
+    data: {
+      type: Object,
+      required: true,
     },
-    activated() {
-        if(typeof this.data.message.isRead !== 'undefined'){
-            this.data.message.isRead = true;
-        }
-    },
-    filters: {
-        formatBytes(bytes){
-            if(bytes == 0){
-                return '0 Bytes';
-            }
-
-            let decimals = 2;
-            let k = 1000;
-            let dm= decimals + 1 || 3;
-            let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-            let i = Math.floor(Math.log(bytes) / Math.log(k));
-
-            return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
-        }
+  },
+  activated() {
+    if (typeof this.data.message.isRead !== "undefined") {
+      this.data.message.isRead = true;
     }
-}
+  },
+  filters: {
+    formatBytes(bytes) {
+      if (bytes == 0) {
+        return "0 Bytes";
+      }
+
+      let decimals = 2;
+      let k = 1000;
+      let dm = decimals + 1 || 3;
+      let sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+      let i = Math.floor(Math.log(bytes) / Math.log(k));
+
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+    },
+  },
+  methods: {
+    navigateBack() {
+      let previousView = this.$parent.previousView;
+
+      eventBus.$emit("changeView", {
+        tag: previousView.tag,
+        title: previousView.title,
+        data: previousView.data,
+      });
+    },
+  },
+};
 </script>
